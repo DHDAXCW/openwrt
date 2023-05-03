412a4c666a1fdb3bdb9afc10d5ae57c2ca224181
骷髅头 <lasstp5011@gmail.com>
2023-05-04 00:04:53 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,6 +18,8 @@ rm -rf package/kernel/rtl8821cu
 rm -rf package/network/services/hostapd
 svn export https://github.com/openwrt/openwrt/trunk/package/kernel/mac80211 package/kernel/mac80211
 svn export https://github.com/openwrt/openwrt/trunk/package/network/services/hostapd package/network/services/hostapd
+rm -rf package/kernel/mac80211/Makefile
+cp -f $GITHUB_WORKSPACE/Makefile package/kernel/mac80211/Makefile
 sed -i '155s/^#//' target/linux/rockchip/image/armv8.mk
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
 sed -i '/uci commit system/i\uci set system.@system[0].hostname='GouPeng'' package/lean/default-settings/files/zzz-default-settings
